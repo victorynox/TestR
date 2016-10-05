3bf2282b74eb75bcb657edbe58533d6012ef9f70
victorynox <it.professor02@gmail.com>
2016-10-05 14:19:22 +0300

    исправлена ошибка со скриптом принимающим уведомления

--- src/Notification/Action/NotificationFactory.php
+++ src/Notification/Action/NotificationFactory.php
@@ -15,7 +15,7 @@ class NotificationFactory
     public function __invoke(ContainerInterface $container, $requestedName)
     {
         $config = $container->get('config');
-        $store = $container->has('ebay_notification') ? $container->get('ebay_notification') : null;
+        $store = $container->has('ebayNotification') ? $container->get('ebayNotification') : null;
         return new NotificationAction($config['ebay'], $store);
     }
 }
